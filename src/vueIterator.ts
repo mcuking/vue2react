c62a5c94a6c547e4b70e21f57c1c81793a06d254
mcuking <mcuking.tang@gmail.com>
2019-05-13 20:52:22 +0800

    feat: support transform vue data like this: 'data: function () { return {a: 1}}'

--- src/vueIterator.ts
+++ src/vueIterator.ts
@@ -18,6 +18,8 @@ export default function vueIterator(vast: t.Node | t.Node[]) {
       if (parent && t.isExportDefaultDeclaration(parent)) {
         switch (name) {
           case 'data':
+            // Support following syntax:
+            // data: () => { return {a: 1}}
             visitor.dataHandler(path.node.body.body, false);
             break;
           case 'created':
@@ -44,16 +46,24 @@ export default function vueIterator(vast: t.Node | t.Node[]) {
             const node = path.node.value;
             if (t.isArrowFunctionExpression(node)) {
               if ((node.body as t.BlockStatement).body) {
+                // Support following syntax:
+                // data: () => { return {a: 1}}
                 visitor.dataHandler(
                   (node.body as t.BlockStatement).body,
                   false
                 );
               } else {
+                // Support following syntax:
+                // data: () => ({a: 1})
                 visitor.dataHandler(
                   (node.body as t.ObjectExpression).properties,
                   true
                 );
               }
+            } else if (t.isFunctionExpression(node)) {
+              // Support following syntax:
+              // data: function () => { return {a: 1}}
+              visitor.dataHandler((node.body as t.BlockStatement).body, false);
             }
             break;
           case 'methods':
