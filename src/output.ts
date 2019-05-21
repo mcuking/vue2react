d8fe97ecc82cb5ffa2b7e6be0cb41212ee210db0
mcuking <tangjianghong@163.com>
2019-05-21 20:24:49 +0800

    feat: 抽象出 output 函数来格式化 生成的 js 代码

---
+++ src/output.ts
@@ -0,0 +1,71 @@
+const format = require('prettier-eslint');
+
+import fs from 'fs';
+
+export default function output(
+  code: string,
+  target: string,
+  isScript: boolean
+) {
+  if (isScript) {
+    const options = {
+      text: code,
+      filePath: target,
+      eslintConfig: {
+        parserOptions: {
+          ecmaVersion: 7,
+          sourceType: 'module',
+          allowImportExportEverywhere: false,
+          ecmaFeatures: {
+            jsx: true,
+            modules: true
+          }
+        },
+        env: {
+          es6: true,
+          node: true,
+          browser: true
+        },
+        rules: {
+          indent: [2, 2, { SwitchCase: 1 }],
+          quotes: [2, 'single', { allowTemplateLiterals: true }],
+          semi: [2, 'always'],
+          eqeqeq: [2, 'always'],
+          strict: [2, 'global'],
+          'object-property-newline': [
+            2,
+            { allowAllPropertiesOnSameLine: false }
+          ],
+          'linebreak-style': [2, 'unix'],
+          'object-curly-newline': [
+            2,
+            {
+              ObjectExpression: 'always',
+              ObjectPattern: 'always'
+            }
+          ],
+          'no-multiple-empty-lines': [2, { max: 0 }],
+          'key-spacing': [2, { afterColon: true }],
+          'block-spacing': [2, 'always'],
+          'space-before-function-paren': [2, 'always'],
+          'padding-line-between-statements': [
+            2,
+            { blankLine: 'always', prev: 'import', next: 'export' }
+          ],
+          'lines-around-comment': [2, { beforeLineComment: true }],
+          'no-console': 0,
+          'no-empty': 0,
+          'no-unused-vars': 0,
+          'no-constant-condition': 0,
+          'no-trailing-spaces': 0
+        }
+      }
+    };
+
+    const formattedCode = format(options);
+    fs.writeFileSync(target, formattedCode);
+    return;
+  }
+
+  fs.writeFileSync(target, code);
+}
