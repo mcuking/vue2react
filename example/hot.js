83edb4b156186f89361f9fdfeae9a2dbff6dafbb
mcuking <mcuking.tang@gmail.com>
2019-05-23 12:32:33 +0800

    feat: vue template 转换 react jsx 对应 demo

--- example/hot.js
+++ example/hot.js
@@ -1,15 +1,16 @@
-import react, { Component } from "react";
-import PropTypes from "PropType";
-import Loader from "./Loader";
-import "index.css";
+import react, { Component } from 'react';
+import PropTypes from 'PropType';
+import Loader from './Loader';
+import 'index.css';
 export default class CoolDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      html: '<div>hello, i am hack</div>',
       show: true,
-      name: "tom",
+      name: 'tom',
       item: {
-        id: "123"
+        id: '123'
       }
     };
   }
@@ -22,45 +23,44 @@ export default class CoolDemo extends Component {
     list3: PropTypes.object
   };
   static defaultProps = {
-    title: "hello world",
+    title: 'hello world',
     list2: [],
     list3: {
-      message: "hello"
+      message: 'hello'
     }
   };
 
   componentWillMount() {
     this.setState({
-      name: "jerry"
+      name: 'jerry'
     });
     this.state.name.we = 1;
   }
 
   componentDidMount() {
     console.log(this.state.name);
-    document.addEventListener("click", this.handlePageClick);
+    document.addEventListener('click', this.handlePageClick);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("click", this.handlePageClick);
+    document.removeEventListener('click', this.handlePageClick);
   }
 
   handlePageClick(e) {
-    const {
-      name
-    } = this;
-    console.log("page click", e, name);
+    const { name } = this;
+    console.log('page click', e, name);
   }
 
   handleTitleClick(e) {
-    const {
-      id
-    } = this.state.item;
-    console.log("title clicked", e, id);
+    const { id } = this.state.item;
+    console.log('title clicked', e, id);
   }
 
   reverseName() {
-    return this.state.name.split("").reverse().join("");
+    return this.state.name
+      .split('')
+      .reverse()
+      .join('');
   }
 
   wrappedTitle() {
@@ -70,7 +70,37 @@ export default class CoolDemo extends Component {
   render() {
     this.reverseName();
     this.wrappedTitle();
-    return;
+    return (
+      <div>
+        {show && (
+          <div
+            a={list}
+            b="1"
+            onClick="handleTitleClick"
+            style={{
+              display: show ? 'block' : 'none'
+            }}
+            dangerouslySetInnerHTML={{
+              __html: html
+            }}
+          >
+            {list.map(item => (
+              <p
+                rt={show}
+                sd="ss"
+                swe={show}
+                tr="23"
+                className="er df"
+                key={item.key}
+              >
+                {item.value}
+              </p>
+            ))}
+            <h1 href="">{reverseName}</h1>
+            <loader />
+          </div>
+        )}
+      </div>
+    );
   }
-
 }
