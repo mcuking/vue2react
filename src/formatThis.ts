2adcf3be10f4baaa6f5765eaa200a5a976817086
mcuking <tangjianghong@163.com>
2019-05-30 20:36:04 +0800

    fix: 修改对 method/computed 方法内部 this 的转换逻辑

--- src/formatThis.ts
+++ src/formatThis.ts
@@ -21,23 +21,43 @@ function getThisIdentify(script: Script, key: string) {
 }
 
 const replaceThisExpression = {
-  // Support following syntax:
-  // this.name -> this.props.name / this.state.name
-  ThisExpression(this: any, tPath: NodePath<t.ThisExpression>) {
-    if (t.isMemberExpression(tPath.parent)) {
+  ThisExpression(this: any, subpath: NodePath<t.ThisExpression>) {
+    if (subpath.parent && t.isMemberExpression(subpath.parent)) {
       // Support following syntax:
-      // const name = this.name;
-      const key = ((tPath.parent as t.MemberExpression)
-        .property as t.Identifier).name;
-      const identify = getThisIdentify(this.script, key);
-      if (identify) {
-        tPath.replaceWith(t.memberExpression(t.thisExpression(), identify));
+      // this.name = 'Tom' -> this.setState({name: 'Tom'})
+      if (
+        subpath.parentPath.parent &&
+        t.isAssignmentExpression(subpath.parentPath.parent) &&
+        subpath.parentPath.parent.left === subpath.parent &&
+        subpath.parentPath.parent.operator === '='
+      ) {
+        (subpath.parentPath.parentPath
+          .parent as t.ExpressionStatement).expression = t.callExpression(
+          t.memberExpression(t.thisExpression(), t.identifier('setState')),
+          [
+            t.objectExpression([
+              t.objectProperty(
+                subpath.parent.property,
+                subpath.parentPath.parent.right
+              )
+            ])
+          ]
+        );
+      } else {
+        // Support following syntax:
+        // const name = this.name -> cosnt name = this.props.name / const name = this.state.name
+        const key = ((subpath.parent as t.MemberExpression)
+          .property as t.Identifier).name;
+        const identify = getThisIdentify(this.script, key);
+        if (identify) {
+          subpath.replaceWith(t.memberExpression(t.thisExpression(), identify));
+        }
       }
-    } else {
-      // todo...
-      // such as const { name } = this;
     }
-    tPath.stop();
+
+    // todo...
+    // such as const { name } = this -> this.props.name / this.state.name
+    subpath.stop();
   }
 };
 
@@ -48,46 +68,30 @@ export default function formatThisExpression(
   let block: t.Statement[] = [];
   path.traverse(
     {
-      VariableDeclaration(path: NodePath<t.VariableDeclaration>) {
+      VariableDeclaration(subpath: NodePath<t.VariableDeclaration>) {
         // Support following syntax:
-        // const name = this.name;
-        path.traverse(replaceThisExpression, { script });
-        block.push(path.node);
-      },
-      ExpressionStatement(path: NodePath<t.ExpressionStatement>) {
-        const expression = path.node.expression;
-        if (t.isCallExpression(expression)) {
-          // Support following syntax:
-          // console.log(this.name);
-          path.traverse(replaceThisExpression, { script });
-        } else if (t.isAssignmentExpression(expression)) {
-          // Not support following syntax:
-          // this.user.name = 'tom'
-          if (
-            t.isMemberExpression(expression.left) &&
-            t.isThisExpression(expression.left.object)
-          ) {
-            // Support following syntax:
-            // this.name = 'tom' -> this.setState({name: 'tom'})
-            path.node.expression = t.callExpression(
-              t.memberExpression(t.thisExpression(), t.identifier('setState')),
-              [
-                t.objectExpression([
-                  t.objectProperty(expression.left.property, expression.right)
-                ])
-              ]
-            );
-          } else {
-            path.traverse(replaceThisExpression, { script });
-          }
+        // const name = this.name -> const name = this.state.name
+        subpath.traverse(replaceThisExpression, { script });
+        if (subpath.parentPath.parent === path.node) {
+          block.push(subpath.node);
         }
-        block.push(path.node);
       },
-      ReturnStatement(path: NodePath<t.ReturnStatement>) {
+      ExpressionStatement(subpath: NodePath<t.ExpressionStatement>) {
         // Support following syntax:
-        // return this.name
-        path.traverse(replaceThisExpression, { script });
-        block.push(path.node);
+        // this.name = 'Tom' -> this.setState({name: 'Tom'})
+        // console.log(this.name) -> console.log(this.state.name) / console.log(this.props.name)
+        subpath.traverse(replaceThisExpression, { script });
+        if (subpath.parentPath.parent === path.node) {
+          block.push(subpath.node);
+        }
+      },
+      ReturnStatement(subpath: NodePath<t.ReturnStatement>) {
+        // Support following syntax:
+        // return this.name -> return this.state.name
+        subpath.traverse(replaceThisExpression, { script });
+        if (subpath.parentPath.parent === path.node) {
+          block.push(subpath.node);
+        }
       }
     },
     { script, block }
