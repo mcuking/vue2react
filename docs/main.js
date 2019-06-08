773d8ceab301e2462d8e3cf83015645d52976349
mcuking <mcuking.tang@gmail.com>
2019-06-08 22:43:00 +0800

    feat: docs 网页版 切换成 typescript + react hooks

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
