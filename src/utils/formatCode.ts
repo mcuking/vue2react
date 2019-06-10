9f3db996397b905b393fc33bedce03f3fb0aead3
mcuking <mcuking.tang@gmail.com>
2019-06-10 14:32:53 +0800

    feat: format vue code before parsed by vue-template-compiler

--- src/utils/formatCode.ts
+++ src/utils/formatCode.ts
@@ -1,27 +1,35 @@
-// import prettier from 'prettier';
 const prettier = require('prettier/standalone');
-// const prettierBabel = require('prettier/parser-babel');
 const prettierBabylon = require('prettier/parser-babylon');
+const prettierHtml = require('prettier/parser-html');
 
-export default function formatCode(code: string) {
-  const options = {
-    parser: 'babel',
-    plugins: [prettierBabylon],
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-    htmlWhitespaceSensitivity: 'css',
-    insertPragma: false,
-    jsxBracketSameLine: false,
-    jsxSingleQuote: false,
-    printWidth: 80,
-    proseWrap: 'preserve',
-    quoteProps: 'as-needed',
-    requirePragma: false,
-    semi: true,
-    singleQuote: false,
-    tabWidth: 2,
-    trailingComma: 'none',
-    useTabs: false
+import { anyObject } from '../types';
+
+export default function formatCode(code: string, type: string) {
+  const options: anyObject = {
+    vue: {
+      parser: 'vue',
+      plugins: [prettierHtml]
+    },
+    react: {
+      parser: 'babel',
+      plugins: [prettierBabylon],
+      arrowParens: 'avoid',
+      bracketSpacing: true,
+      htmlWhitespaceSensitivity: 'css',
+      insertPragma: false,
+      jsxBracketSameLine: false,
+      jsxSingleQuote: false,
+      printWidth: 80,
+      proseWrap: 'preserve',
+      quoteProps: 'as-needed',
+      requirePragma: false,
+      semi: true,
+      singleQuote: false,
+      tabWidth: 2,
+      trailingComma: 'none',
+      useTabs: false
+    }
   };
-  return prettier.format(code, options);
+
+  return prettier.format(code, options[type]);
 }
